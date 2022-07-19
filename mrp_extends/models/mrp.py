2f664167c6114153e78e4fb4157a5fcf9c66759b
renzobee <renzo@mybusybee.net>
2022-07-19 11:41:38 +0800

    [imp] fix tweaks for mrp extends report cost

--- mrp_extends/models/mrp.py
+++ mrp_extends/models/mrp.py
@@ -4,6 +4,16 @@ from odoo import models, fields, api
 from odoo.tools.float_utils import float_round
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
+
+    @api.onchange('bom_line_ids')
+    def _compute_product_qty(self):
+        for bom in self:
+            for line in bom.bom_line_ids:
+                percent = (line.quantity_amount * line.decrease_percent) / 100
+                line.product_qty = line.quantity_amount - percent
+
 
 class MrpBomLine(models.Model):
     _inherit = 'mrp.bom.line'
@@ -11,10 +21,4 @@ class MrpBomLine(models.Model):
     quantity_amount = fields.Float(string="Quantity", default=1.0,)
     decrease_percent = fields.Float(string="Decrease %")
     product_qty = fields.Float(
-        'Quantity', digits='Product Unit of Measure', required=True, compute='_compute_product_qty', store=True)
-
-    @api.depends('product_qty', 'decrease_percent')
-    def _compute_product_qty(self):
-        for line in self:
-            percent = (line.quantity_amount * line.decrease_percent) / 100
-            line.product_qty = line.quantity_amount - percent
+        'Quantity', digits='Product Unit of Measure', required=True, store=True)
